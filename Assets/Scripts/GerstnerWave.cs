3c0aaac3d760ec969495a7247a8f7973012a6e6d
Bowen Tao <bowentao2015@gmail.com>
2019-09-21 20:42:02 +0800

    unity upgrade

---
+++ Assets/Scripts/GerstnerWave.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerstnerWave : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    Vector3[] dest;
+    [Range(0,1)]
+    public float steepness;
+    public float amplitude;
+    [Range(1,50)]
+    public int[] period;
+    [Range(0.5f,10f)]
+    public float[] speed;
+    [SerializeField]
+    public Vector2[] waveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        dest = new Vector3[vertices.Length];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waveDir.Length != speed.Length || waveDir.Length != period.Length){
+            Debug.LogError("Variables set not correct");
+            return;
+        }
+        for (int i=0;i<vertices.Length;i++){
+            Vector3 v = vertices[i];
+            for (int j=0;j<waveDir.Length;j++){
+                float wi = (2 * Mathf.PI) / period[j];
+                float inner = wi * Vector2.Dot(waveDir[j],new Vector2(v.x,v.z)) + speed[j] * Time.time;
+                float Qi = steepness / wi;
+                v.x += Qi * waveDir[j].x * Mathf.Cos(inner);
+                v.z += Qi * waveDir[j].y * Mathf.Cos(inner);
+                v.y += amplitude * Mathf.Sin(inner);
+            }
+            dest[i] = v;
+        }
+        mesh.vertices = dest;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+}
---
+++ Assets/Scripts/ProceduralGrid.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGrid : MonoBehaviour
+{
+    public int xGrid = 1;
+    public int yGrid = 1;
+    Mesh m_mesh;
+    public Vector2 size = Vector2.one;
+
+    void Awake() {
+        Generate();
+    }
+
+    void OnValidate() {
+        Generate();
+    }
+
+    private void Generate(){
+        if (xGrid < 1 || yGrid < 1){
+            Debug.LogError("xGrid and yGrid must be positive int");
+            return;
+        }
+        m_mesh = new Mesh();
+        m_mesh.name = "MyMesh";
+        int count = (xGrid + 1) * (yGrid + 1);
+        Vector3[] vertices = new Vector3[count];
+        Vector2[] uv = new Vector2[count];
+        int[] triangles = new int[xGrid * yGrid * 6];
+
+        for (int vIdx=0,y = 0;y<=yGrid;y++){
+            for (int x=0;x<=xGrid;x++,vIdx++){
+                vertices[vIdx] = new Vector3(size.x * ((float)x / xGrid - 0.5f), 0, size.y * ((float)y / yGrid - 0.5f));
+                uv[vIdx] = new Vector2((float)x/xGrid,(float)y/yGrid);
+            }
+        }
+        for (int vIdx=0,tIdx=0,y=0;y<yGrid;y++,vIdx++){
+            for (int x=0;x<xGrid;x++,vIdx++,tIdx+=6){
+                triangles[tIdx] = vIdx;
+                triangles[tIdx+1] = triangles[tIdx+4] = vIdx+xGrid+1;
+                triangles[tIdx+2] = triangles[tIdx+3] = vIdx+1;
+                triangles[tIdx+5] = vIdx + xGrid + 2;
+            }
+        }
+        
+        m_mesh.vertices = vertices;
+        m_mesh.uv = uv;
+        m_mesh.triangles = triangles;
+        m_mesh.RecalculateBounds();
+        m_mesh.RecalculateNormals();
+        // m_mesh.RecalculateTangents();
+
+        GetComponent<MeshFilter>().mesh = m_mesh;
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+        if (meshCollider != null){
+            meshCollider.sharedMesh = m_mesh;
+        }
+
+                
+    }
+}
