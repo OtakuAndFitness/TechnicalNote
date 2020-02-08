95b32cbd86846983c6457669b9ba49e747687f24
Bowen Tao <bowentao2015@gmail.com>
2020-02-08 15:51:19 +0800

    compute shader example

--- Assets/Scripts/CSExample.cs
+++ Assets/Scripts/CSExample.cs
@@ -1,18 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CSExample : MonoBehaviour
 {
+    public bool isTextureMode = true;
+    public Texture inputTex;
+    public ComputeShader computeShader;
+    public ComputeShader csBuffer;
+    public RawImage image;
+    ComputeBuffer buffer;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        if (isTextureMode){
+            GameObject.Find("Canvas").SetActive(true);
+            TextureGray();
+        }else{
+            GameObject.Find("Canvas").SetActive(false);
+            CSFib();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void TextureGray(){
+        RenderTexture t = new RenderTexture(inputTex.width,inputTex.height,24);
+        t.enableRandomWrite = true;
+        t.Create();
+        image.texture = t;
+        image.SetNativeSize();
+
+        int kernel = computeShader.FindKernel("Gray");
+        computeShader.SetTexture(kernel,"inputTexture",inputTex);
+        computeShader.SetTexture(kernel,"outputTexture",t);
+        computeShader.Dispatch(kernel,inputTex.width / 8, inputTex.height / 8,1);
+    }
+
+    public void CSFib(){
+        int[] total = new int[32];
+        buffer = new ComputeBuffer(32,sizeof(int));
+        int kernel = csBuffer.FindKernel("Fibonacci");
+        csBuffer.SetBuffer(kernel,"buffer",buffer);
+        csBuffer.Dispatch(kernel,1,1,1);
+        buffer.GetData(total);
+        for (int i = 0; i < total.Length; i++)
+        {
+            Debug.Log(total[i]);
+        }
+
+    }
+
+    private void OnDestroy() {
+        if (!isTextureMode){
+            buffer.Release();
+        }
     }
 }
