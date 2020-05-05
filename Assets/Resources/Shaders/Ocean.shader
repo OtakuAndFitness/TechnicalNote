b3fb8c72bdf892a6b06431159d2988a136d7645c
Bowen Tao <bowentao2015@gmail.com>
2020-05-05 11:31:16 +0800

    water in progress

---
+++ Assets/Resources/Shaders/Ocean.shader
@@ -0,0 +1,111 @@
+﻿Shader "Unlit/Ocean"
+{
+    Properties
+    {
+        _Color("Main Color",Color) = (1,1,1,1)
+        _HeightTex("Height Texture",2D) = "white"{}
+        _DisplaceTex("Displacement Texture",2D) = "whhite"{}
+        _NormalTex("Normal Texture",2D) = "bump"{}
+        _BubbleTex("Bubble Texture",2D) = "white"{}
+        _LightWrap ("Light Wrapping Value", Float) = 1 
+		_SpecularColor ("Specular Color", Color) = (1, 0.25, 0, 1)
+		_Glossiness ("Glossiness", Float) = 64
+
+
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "LightMode" = "ForwardBase"}
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #include "Lighting.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uvHeight : TEXCOORD0;
+                float2 uvDisplace : TEXCOORD1;
+                float2 uvBubble : TEXCOORD2;
+                float2 uvNormal : TEXCOORD3;
+            };
+
+            struct v2f
+            {
+                float2 uvHeight : TEXCOORD0;
+                float2 uvDisplace : TEXCOORD3;
+                float2 uvBubble : TEXCOORD4;
+                float2 uvNormal : TEXCOORD5;
+
+                float4 vertex : SV_POSITION;
+                float4 color : TEXCOORD1;
+                float4 posWorld : TEXCOORD2;
+            };
+
+            sampler2D _HeightTex;
+            float4 _HeightTex_ST;
+            sampler2D _DisplaceTex;
+            float4 _DisplaceTex_ST;
+            sampler2D _NormalTex;
+            float4 _NormalTex_ST;
+            sampler2D _BubbleTex;
+            float4 _BubbleTex_ST;
+            float4 _Color;
+            float _LightWrap;
+            float4 _SpecularColor;
+            float _Glossiness;
+
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+
+                o.uvHeight = TRANSFORM_TEX(v.uvHeight,_HeightTex);
+                o.uvDisplace = TRANSFORM_TEX(v.uvDisplace,_DisplaceTex);
+                o.uvBubble = TRANSFORM_TEX(v.uvBubble,_BubbleTex);
+                o.uvNormal = TRANSFORM_TEX(v.uvNormal,_NormalTex);
+
+                float4 pos = v.vertex;
+                pos.y += tex2Dlod(_HeightTex,float4(v.uvHeight,0,0)).r + tex2Dlod(_HeightTex,float4(v.uvHeight,0,0)).b;
+                pos.xz += tex2Dlod(_DisplaceTex,float4(v.uvDisplace,0,0)).rb;
+                o.vertex = UnityObjectToClipPos(pos);
+                o.posWorld = mul(unity_ObjectToWorld,pos);
+
+                o.color = tex2Dlod(_BubbleTex,float4(v.uvBubble,0,0)).r;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                
+                float3 normal = normalize(UnityObjectToWorldNormal(tex2Dlod(_NormalTex, float4(i.uvNormal,0,0)).rgb));
+
+				// float3 lightDir = normalize(lerp(_WorldSpaceLightPos0.xyz, _WorldSpaceLightPos0.xyz - i.posWorld.xyz, _WorldSpaceLightPos0.w));
+				float3 lightDir = normalize(_WorldSpaceLightPos0.xyz - i.posWorld.xyz);
+
+                float3 view = normalize(_WorldSpaceCameraPos.xyz - i.posWorld.xyz);
+
+                float4 diffuse = saturate(dot(normal, lightDir));
+				diffuse = pow(saturate(diffuse * (1 - _LightWrap) + _LightWrap), 2 * _LightWrap + 1) * _Color * _LightColor0;
+
+				float3 H = normalize(view + lightDir);
+				float NdotH = saturate(dot(normal, H));
+				float4 specular = _SpecularColor * saturate(pow(NdotH, _Glossiness)) * _LightColor0;
+
+				// float4 rim = _RimColor * pow(max(0, 1 - saturate(dot(normal, view))), 1.5);
+
+				float4 white = saturate(pow(i.color / 2, 2));
+
+				return diffuse;// + specular + white;
+
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Scripts/FFTOcean.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FFTOcean : MonoBehaviour
+{
+    public ComputeShader fftOcean;
+    public Material oceanMat;
+    public int resolution = 128;
+    public int detail = 8;
+    public int unit = 1;
+    public int Length = 10;
+    public float A = 0.0001f;
+    public Vector2 wind;
+    public float timeScale = 1.0f;
+    private int N;
+    private float time;
+    private MeshFilter filter;
+    private Mesh mesh;
+    private RenderTexture HeightTex;
+    private RenderTexture DispaceTex;
+    private RenderTexture NormalTex;
+    private RenderTexture BubbleTex;
+    private RenderTexture OutputRT;
+    private int kernelInitHeightSpectrum;
+    private int kernelInitDisplaceSpectrum;
+    private int kernelInitNormalAndBubble;
+    private int kernelHorizontalFFT;
+    private int kernelVerticalFFT;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject ocean = new GameObject("Ocean");
+        filter = ocean.AddComponent<MeshFilter>();
+        mesh = new Mesh();
+        filter.mesh = mesh;
+
+        ocean.AddComponent<MeshRenderer>().material = oceanMat;
+
+        GenerateMesh();
+        InitCSValue();
+
+    }
+
+    #region MeshCreate
+    void GenerateMesh(){
+        Vector3[] vertices = new Vector3[resolution * resolution];
+        Vector3[] normals = new Vector3[resolution * resolution];
+        Vector2[] uvs = new Vector2[resolution * resolution];
+        int[] indices = new int[(resolution - 1) * (resolution - 1) * 6];
+
+        int indicesIndex = 0;
+        int halfResolution = resolution / 2;
+
+        for (int i = 0; i < resolution; i++)
+        {
+            float horizontalPos = (i - halfResolution) * unit;
+            for (int j = 0; j < resolution; j++)
+            {
+                int currentIndex = i * resolution + j;
+                float verticalPos = (j - halfResolution) * unit;
+                vertices[currentIndex] = new Vector3(horizontalPos,0,verticalPos);
+                normals[currentIndex] = new Vector3(0,1,0);
+                uvs[currentIndex] = new Vector2(i / (resolution - 1),j / (resolution-1));
+
+                if (i != resolution - 1 && j != resolution - 1){
+                    indices[indicesIndex++] = currentIndex;
+                    indices[indicesIndex++] = currentIndex + 1;
+                    indices[indicesIndex++] = currentIndex + resolution + 1;
+
+                    indices[indicesIndex++] = currentIndex;
+                    indices[indicesIndex++] = currentIndex + resolution + 1;
+                    indices[indicesIndex++] = currentIndex + resolution;
+                }
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.SetIndices(indices,MeshTopology.Triangles,0);
+        mesh.normals = normals;
+        mesh.uv = uvs;
+        filter.mesh = mesh;
+
+    }
+    #endregion
+
+    #region InitCSValue
+    void InitCSValue(){
+        N = resolution * detail;
+        HeightTex = CreateRT(N);
+        DispaceTex = CreateRT(N);
+        NormalTex = CreateRT(N);
+        BubbleTex = CreateRT(N);
+        OutputRT = CreateRT(N);
+        
+        kernelInitHeightSpectrum = fftOcean.FindKernel("InitHeightSpectrum");
+        kernelInitDisplaceSpectrum = fftOcean.FindKernel("InitDisplaceSpectrum");
+        kernelInitNormalAndBubble = fftOcean.FindKernel("InitNormalAndBubbleTex");
+        kernelHorizontalFFT = fftOcean.FindKernel("HorizontalFFT");
+        kernelVerticalFFT = fftOcean.FindKernel("VerticalFFT");
+
+        fftOcean.SetInt("N",N);
+        fftOcean.SetInt("Length",Length);
+
+        fftOcean.SetFloat("randomVal1",Random.value);
+        fftOcean.SetFloat("randomVal2",Random.value);
+
+    }
+
+    private RenderTexture CreateRT(int size){
+        RenderTexture rt = new RenderTexture(size,size,0,RenderTextureFormat.ARGBFloat);
+        rt.enableRandomWrite = true;
+        rt.Create();
+        return rt;
+    }
+    #endregion
+
+    #region UpdateOceanValue
+    void UpdateOceanValue(float time){
+        //初始化各个参数
+        fftOcean.SetFloat("A",A);
+        fftOcean.SetFloat("time",time);
+        fftOcean.SetVector("wind",wind);
+
+        //初始化高度频谱
+        fftOcean.SetTexture(kernelInitHeightSpectrum,"HeightSpectrumRT",HeightTex);
+        fftOcean.Dispatch(kernelInitHeightSpectrum,N/8,N/8,1);
+
+        //初始化偏移频谱
+        fftOcean.SetTexture(kernelInitDisplaceSpectrum,"HeightSpectrumRT",HeightTex);
+        fftOcean.SetTexture(kernelInitDisplaceSpectrum,"DisplaceSpectrumRT",DispaceTex);
+        fftOcean.Dispatch(kernelInitDisplaceSpectrum,N/8,N/8,1);
+
+        int iterations = Mathf.CeilToInt(Mathf.Log(N,2));
+        
+        for(int i=1;i<=iterations;i++){// horizontal fft
+            int ns = (int)Mathf.Pow(2,i-1);
+            fftOcean.SetInt("Ns",ns * 2);
+            fftOcean.SetInt("iteration",i);
+            FFT(kernelHorizontalFFT, ref HeightTex);
+            FFT(kernelHorizontalFFT, ref DispaceTex);
+        }
+
+        for(int i=1;i<=iterations;i++){//vertical fft
+            int ns = (int)Mathf.Pow(2,i-1);
+            fftOcean.SetInt("Ns",ns * 2);
+            fftOcean.SetInt("iteration",i);
+            FFT(kernelVerticalFFT, ref HeightTex);
+            FFT(kernelVerticalFFT, ref DispaceTex);
+        }
+
+        //初始化法线以及泡沫
+        // fftOcean.SetTexture(kernelInitNormalAndBubble,"DisplaceSpectrumRT",DispaceTex);
+        // fftOcean.SetTexture(kernelInitNormalAndBubble,"NormalRT",NormalTex);
+        // fftOcean.SetTexture(kernelInitNormalAndBubble,"BubbleRT",BubbleTex);
+        // fftOcean.Dispatch(kernelInitNormalAndBubble,N/8,N/8,1);
+
+        SetMaterial();
+    }
+
+    private void FFT(int kernel, ref RenderTexture input){ 
+        fftOcean.SetTexture(kernel,"InputRT",input);
+        fftOcean.SetTexture(kernel,"OutputRT",OutputRT);
+        fftOcean.Dispatch(kernel,N/8,N/8,1);
+
+        RenderTexture rt = input;
+        input = OutputRT;
+        OutputRT = rt;
+
+    }
+
+    private void SetMaterial(){
+        oceanMat.SetTexture("_HeightTex",HeightTex);
+        oceanMat.SetTexture("_DisplaceTex",DispaceTex);
+        oceanMat.SetTexture("_NormalTex",NormalTex);
+        oceanMat.SetTexture("_BubbleTex",BubbleTex);
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime * timeScale;
+        UpdateOceanValue(time);
+    }
+    #endregion
+
+
+    void OnDestroy() {
+        HeightTex.Release();
+        DispaceTex.Release();
+        NormalTex.Release();
+        BubbleTex.Release();
+        OutputRT.Release();
+    }
+}
